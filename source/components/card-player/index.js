16a426cef25302f970c34fe83d94dd45038fdfbb
Eric Elliott <support@paralleldrive.com>
2016-03-14 01:13:16 -0600

    Flesh out the CardPlayer element
    * Add presence tests for all major UI pieces
    * Disable continue button if isCompleted is false

--- source/components/card-player/index.js
+++ source/components/card-player/index.js
@@ -1,3 +1,20 @@
-export default React => ({ courseTitle }) => (
-  <h1 className="course-title">{ courseTitle }</h1>
+export default React => ({ courseTitle, cardContent, isCompleted }) => (
+  <div className="card-player">
+    <div className="nav-bar">
+      <h1 className="course-title">{ courseTitle }</h1>
+      <nav className="nav">
+        <select>
+          <option>Card 1</option>
+          <option>Card 2</option>
+          <option>Card 3</option>
+        </select>
+      </nav>
+    </div>
+    <div className="card-content">{ cardContent}</div>
+    <div className="next-card">
+      <div className="spacer"></div>
+      <button className="button"
+        disabled={ isCompleted ? '' : 'disabled'}>Continue</button>
+    </div>
+  </div>
 );
