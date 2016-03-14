249793ee8dd108ade47ff218c3492247d42b83f4
Eric Elliott <support@paralleldrive.com>
2016-03-13 22:16:02 -0600

    Initial code commit
    * Set up unit tests
    * Implement the first component unit test

---
+++ source/components/card-player/index.js
@@ -0,0 +1,3 @@
+export default React => ({ courseTitle }) => (
+  <h1 className="course-title">{ courseTitle }</h1>
+);
--- source/test/index.js
+++ source/test/index.js
@@ -1,2 +1,28 @@
+import React from 'react';
 import test from 'tape';
-import card from 'components/card';
+import dom from 'cheerio';
+import reactDom from 'react-dom/server';
+
+const render = reactDom.renderToStaticMarkup;
+
+import cardPlayer from 'components/card-player';
+
+test('CardPlayer', assert => {
+  const msg = 'CardPlayer should render the course title.';
+
+  const CardPlayer = cardPlayer(React);
+
+  const courseTitle = 'My Course';
+  const props = {
+    courseTitle
+  };
+
+  const $ = dom.load(render(<CardPlayer { ...props } />));
+  const output = $('.course-title').html();
+
+  const actual = output;
+  const expected = courseTitle;
+
+  assert.equal(actual, expected, msg);
+  assert.end();
+});
