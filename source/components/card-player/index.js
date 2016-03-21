0822c833ef145e3e399052fd580d623ce55fdffe
Eric Elliott <support@paralleldrive.com>
2016-03-21 02:47:03 -0600

    Iterate over card-list items
    * Map over child elements
    * Separate child element component
    * Simple factory for test data
    * Easy conditional classnames

--- source/components/card-player/index.js
+++ source/components/card-player/index.js
@@ -1,20 +1,36 @@
-export default React => ({ courseTitle, cardContent, isCompleted }) => (
-  <div className="card-player">
-    <div className="nav-bar">
-      <h1 className="course-title">{ courseTitle }</h1>
-      <nav className="nav">
-        <select>
-          <option>Card 1</option>
-          <option>Card 2</option>
-          <option>Card 3</option>
-        </select>
-      </nav>
+const makeClasses = (...classes) => {
+  return classes.join(' ').trim();
+};
+
+export default React => {
+
+  const CardListItem = ({ card, currentCardId }) => (
+    <li key={ card.id }
+      className={
+        makeClasses('card-list-item',
+          card.id === currentCardId ? 'current-card' : ''
+        )
+      }>{ card.name }</li>
+  );
+
+  return ({ courseTitle, cardContent, isCompleted, cardList, currentCardId }) => (
+    <div className="card-player">
+      <div className="nav-bar">
+        <h1 className="course-title">{ courseTitle }</h1>
+      </div>
+      <div className="card-container">
+        <div className="card-content">{ cardContent }</div>
+        <div className="next-card">
+          <div className="spacer"></div>
+          <button className="button"
+            disabled={ isCompleted ? '' : 'disabled' }>Continue</button>
+        </div>
+      </div>
+      <ul className="card-list">
+        {
+          cardList.map(card => CardListItem({ card, currentCardId }))
+        }
+      </ul>
     </div>
-    <div className="card-content">{ cardContent }</div>
-    <div className="next-card">
-      <div className="spacer"></div>
-      <button className="button"
-        disabled={ isCompleted ? '' : 'disabled' }>Continue</button>
-    </div>
-  </div>
-);
+  );
+};
--- source/test/index.js
+++ source/test/index.js
@@ -7,16 +7,34 @@ const render = reactDom.renderToStaticMarkup;
 
 import cardPlayer from 'components/card-player';
 
+const makeProps = (settings) => Object.assign({}, {
+  courseTitle: 'My Course',
+  cardContent: 'Contents...',
+  isCompleted: false,
+  cardList: [
+    {
+      id: '1',
+      name: 'Card 1'
+    },
+    {
+      id: '2',
+      name: 'Card 2'
+    },
+    {
+      id: '3',
+      name: 'Card 3'
+    }
+  ],
+  currentCardId: '1'
+}, settings);
+
 test('CardPlayer', nest => {
   nest.test('...card-player', assert => {
     const msg = 'CardPlayer should render the card-player div.';
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const props = makeProps();
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.card-player').children().length;
@@ -33,10 +51,7 @@ test('CardPlayer', nest => {
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const props = makeProps();
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.nav-bar').children().length;
@@ -53,10 +68,8 @@ test('CardPlayer', nest => {
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const courseTitle = 'My Amazing Course';
+    const props = makeProps({ courseTitle });
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.course-title').html();
@@ -68,37 +81,15 @@ test('CardPlayer', nest => {
     assert.end();
   });
 
-  nest.test('...nav', assert => {
-    const msg = 'CardPlayer should render the nav.';
-
-    const CardPlayer = cardPlayer(React);
-
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
-
-    const $ = dom.load(render(<CardPlayer { ...props } />));
-    const output = $('.nav').children().length;
-
-    const actual = output > 0;
-    const expected = true;
-
-    assert.equal(actual, expected, msg);
-    assert.end();
-  });
-
   nest.test('...card-content', assert => {
     const msg = 'CardPlayer should render the card-content.';
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
     const cardContent = 'My content';
-    const props = {
-      courseTitle,
+    const props = makeProps({
       cardContent
-    };
+    });
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.card-content').html();
@@ -115,10 +106,7 @@ test('CardPlayer', nest => {
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle
-    };
+    const props = makeProps();
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.next-card').children().length;
@@ -135,11 +123,7 @@ test('CardPlayer', nest => {
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle,
-      isCompleted: false
-    };
+    const props = makeProps({ isCompleted: false });
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.next-card .button').attr('disabled');
@@ -156,11 +140,7 @@ test('CardPlayer', nest => {
 
     const CardPlayer = cardPlayer(React);
 
-    const courseTitle = 'My Course';
-    const props = {
-      courseTitle,
-      isCompleted: true
-    };
+    const props = makeProps({ isCompleted: true });
 
     const $ = dom.load(render(<CardPlayer { ...props } />));
     const output = $('.next-card .button').attr('disabled');
@@ -171,4 +151,91 @@ test('CardPlayer', nest => {
     assert.equal(actual, expected, msg);
     assert.end();
   });
+
+  nest.test('...card-container', assert => {
+    const msg = 'CardPlayer should render the card-container.';
+
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps();
+
+    const $ = dom.load(render(<CardPlayer { ...props } />));
+    const output = $('.card-container').children().length;
+
+    const actual = output > 0;
+    const expected = true;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+
+  nest.test('...card-list', assert => {
+    const msg = 'CardPlayer should render the card-list.';
+
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps();
+
+    const $ = dom.load(render(<CardPlayer { ...props } />));
+    const output = $('.card-list').children().length;
+
+    const actual = output > 0;
+    const expected = true;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+
+  nest.test('...card-list contents', assert => {
+    const msg = 'CardPlayer should render the correct card-list.';
+
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps({
+      cardList: [
+        {
+          id: '1',
+          name: 'Card #1'
+        },
+        {
+          id: '2',
+          name: 'Card #2'
+        },
+        {
+          id: '3',
+          name: 'Card #3'
+        }
+      ]
+    });
+
+    const $ = dom.load(render(<CardPlayer { ...props } />));
+    const output = $('.card-list').html();
+
+    const actual = output;
+    const expected = '<li class="card-list-item current-card">Card #1</li><li class="card-list-item">Card #2</li><li class="card-list-item">Card #3</li>';
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+
+
+  nest.test('...card-list current card', assert => {
+    const msg = 'CardPlayer should render the current card.';
+
+    const CardPlayer = cardPlayer(React);
+
+    const props = makeProps({
+      currentCardId: '3'
+    });
+
+    const $ = dom.load(render(<CardPlayer { ...props } />));
+    const output = $('.card-list').children().last().attr('class');
+
+    const actual = (/current-card/).test(output);
+    const expected = true;
+
+    assert.equal(actual, expected, msg);
+    assert.end();
+  });
+
 });
